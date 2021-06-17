229079d1eaec75ecf53ce99caa67e8d1fec04428
juyeongsung <jooyongs1@gmail.com>
2021-06-17 16:37:35 +0900

    17일 오후

--- json.js
+++ json.js
@@ -1,14 +1,32 @@
-let json = JSON.stringify(true);
-console.log(json)
+// stringify 방법
+
+// let json = JSON.stringify(true);
+// json = JSON.stringify(['banana','apple'])
+// console.log(json)
+const robot = {
+  name:"tori",
+  color:"white",
+  size:null,
+  birthDate:new Date(),
+  jump:()=>{
+    console.log(`${name} can jump!`)
+  }
+}
+// json = JSON.stringify(robot, (key,value)=>{ return value})
+// console.log(json)
 
 
+// parse 방법
 
-
+json= JSON.stringify(robot);
+const obj = JSON.parse(json);
+console.log(obj)
 
 /*
-JSON
+JSON(string, number, object, array, true, false, null을 지원)
   1. Object -> JSON : .stringify
-
+    string으로 변환된다
+    함수는 변환되지 않는다
   2. JSON -> Object : .parse
 
 */
